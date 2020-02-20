a5e422edef9dddddc9cb097c585ffd753d73e2dd
senbiki akifumi <a-senbiki@sunmark.bz>
2020-02-20 11:02:46 +0900

    Laravelのインストール完了

---
+++ app-source/system/app/Http/Controllers/TopController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TopController extends Controller
+{
+
+
+    public function index()
+    {
+        printf("Laravel インストール 環境");
+    }
+}
